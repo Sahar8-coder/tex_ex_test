67b2882bacd0763c443e82a650b7f682fc579d01
Sahar8-coder <ze.kirilov@gmail.com>
2023-01-04 16:43:17 +0200

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import MathJax from 'react-mathjax2'
+ 
+const tex = `f(x) = \\int_{-\\infty}^\\infty\\hat f(\\xi)\\,e^{2 \\pi i \\xi x}\\,d\\xi`
+ 
+export default function MyApp () {
+    return (
+        <div>
+            <MathJax.Context input='tex'>
+                <div>
+                    This is an inline math formula: <MathJax.Node inline>{'a = b'}</MathJax.Node>
+                </div>
+            </MathJax.Context>
+        </div>
+    );
 }
-
-export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-/*import MathJax from 'react-mathjax2'
+import MathJax from 'react-mathjax2'
  
 const tex = `f(x) = \\int_{-\\infty}^\\infty\\hat f(\\xi)\\,e^{2 \\pi i \\xi x}\\,d\\xi`
- */
-export default function index () {
+ 
+export default function test () {
     return (
         <div>
-            HELLO WORLD
+            <MathJax.Context input='tex'>
+                <div>
+                    This is an inline math formula: <MathJax.Node inline>{'a = b'}</MathJax.Node>
+                </div>
+            </MathJax.Context>
         </div>
     );
 }
