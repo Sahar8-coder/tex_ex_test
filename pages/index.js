f1ef936d395987cc3b1220ce093913d8a3e97a50
Sahar8-coder <ze.kirilov@gmail.com>
2023-01-04 16:38:33 +0200

    .

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-import MathJax from 'react-mathjax2'
+/*import MathJax from 'react-mathjax2'
  
 const tex = `f(x) = \\int_{-\\infty}^\\infty\\hat f(\\xi)\\,e^{2 \\pi i \\xi x}\\,d\\xi`
- 
-export default function test () {
+ */
+export default function index () {
     return (
         <div>
-            <MathJax.Context input='tex'>
-                <div>
-                    This is an inline math formula: <MathJax.Node inline>{'a = b'}</MathJax.Node>
-                </div>
-            </MathJax.Context>
+            HELLO WORLD
         </div>
     );
 }
